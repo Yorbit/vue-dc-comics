0a15a0dba3e1887990b509d76861e8c02523c563
Yorbit <marco.95112@hotmail.it>
2022-07-22 14:18:08 +0200

    add empty main base file

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <div id="app">
-  </div>
+  <section id="app">
+    <BaseHeader/>
+    <BaseMain/>
+    <BaseFooter/>
+  </section>
 </template>
 
 <script>
 
+import BaseHeader from './components/BaseHeader.vue'
+import BaseMain from './components/BaseMain.vue'
+import BaseFooter from './components/BaseFooter.vue'
+
+
 export default {
   name: 'App',
   components: {
+    BaseHeader,
+    BaseMain,
+    BaseFooter,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import'./styles/general.scss';
+  @import'./styles/colors.scss';
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+};
 </style>

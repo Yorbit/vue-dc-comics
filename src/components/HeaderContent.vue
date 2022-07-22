6e099ca94a7bdc146ed86d0ad03283445b49c453
Yorbit <marco.95112@hotmail.it>
2022-07-22 15:58:40 +0200

    ad black placeholder

---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,20 @@
+<template>
+      <div class="container-cont">
+            <h3>--CONTENT GOES HERE--</h3>
+      </div>
+
+</template>
+
+<style lang="scss" scoped>
+  @import'../styles/colors.scss';
+
+      .container-cont {
+          color: white;
+          background-color: $BannerBg;
+          width: 1900px;
+          margin: 0 auto;
+          justify-content: space-between;
+          align-items: center;
+          padding: 50px 250px;
+        }
+</style>
